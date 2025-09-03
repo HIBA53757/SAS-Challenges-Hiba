cc649e97c8cc17cb7349e13027f57e41ab1adb0c
HIBA <hibatallahsadiki20@gmail.com>
2025-09-03 17:24:23 +0200

    Add files via upload

---
+++ condition challenge 2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{  char n;
+    printf("Entrer un caractere:");
+    scanf("%c",&n);
+    switch(n){
+ case a:
+    printf("le caractère saisi est une voyelle");
+ break;
+ case e :
+    printf("le caractère saisi est une voyelle");
+ break;
+  case o:
+    printf("le caractère saisi est une voyelle");
+ break;
+  case u:
+    printf("le caractère saisi est une voyelle");
+ break;
+  case i:
+    printf("le caractère saisi est une voyelle");
+ break;
+  default:
+    printf("non voyelle");
+    }
+    return 0;
+}

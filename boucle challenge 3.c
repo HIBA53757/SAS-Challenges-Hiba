29ccd28fea37ef569e4a87003f9982a4a9db3fa6
HIBA <hibatallahsadiki20@gmail.com>
2025-09-04 15:55:40 +0200

    Add files via upload

---
+++ boucle challenge 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int n,i,s;
+       printf("enterer un nombre:");
+       scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        s=(n*(n+1))/2;}
+        printf("Nombres Naturels de %d est:%d",n,s);
+
+    return 0;
+}

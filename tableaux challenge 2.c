2eea69800e4816f3030b27e9b9d40fdb1456a9bc
HIBA <hibatallahsadiki20@gmail.com>
2025-09-08 12:16:10 +0100

    Add files via upload

---
+++ tableaux challenge 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int i,taille;
+    printf("entrer la taille:");
+    scanf("%d",&taille);
+    int T[taille];
+for (i=0;i<taille;i++){
+    printf("veuiller enter les element du tableau:");
+    scanf("%d",&T[taille]);
+}
+    return 0;
+}

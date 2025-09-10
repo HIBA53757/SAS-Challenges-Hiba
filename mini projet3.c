eaa0b8c297c22027d803c22b26a734cedf1a449c
HIBA <hibatallahsadiki20@gmail.com>
2025-09-10 10:33:31 +0100

    Add files via upload

---
+++ mini projet3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{    char positif[3][100]={"I am feeling happy","Hi! Great to hear that!"};
+     char negatif[3][100]={"I am sad today","Oh no! Hope things get better soon."};
+     char neutre[3][100]={"Can you help me?","show history","Sure! What do you want to talk about?"};
+
+
+
+     char n[100];
+
+
+     while (1){
+      printf("\nhow can i help you today?\n");
+      scanf("%s",n);}
+      if (){
+      printf("Hi! Great to hear that!");
+      scanf("%s",n);
+}
+    return 0;
+}
